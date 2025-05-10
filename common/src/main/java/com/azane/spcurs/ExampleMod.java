897eb81380152a19f59fcc7a4977c4bd435968b9
azaneNH37 <Anventie37000631@outlook.com>
2025-05-11 00:57:04 +0800

    init: Architectury template

---
+++ common/src/main/java/com/azane/spcurs/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.azane.spcurs;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "spawncursion";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
